f3fef0937cce2a96ada9f62b3f06f090c10a36a7
mubzie <rabiumubarak92@gmail.com>
2022-05-26 23:46:58 +0100

    worked on functionality that put player marker on the gameboard

--- script.js
+++ script.js
@@ -1,21 +1,115 @@
-//target html gameboard DOM
-const boards = document.querySelectorAll(".cell");
+  // DOM selection
+  const gameBoard = document.querySelector("#gameboard-container");
+  const cells = document.querySelectorAll(".cell");
 
+//player factory function
+const Player = (name, marker) => {
+    const getMark = () => marker;
+    const getName = () => name;
+    return{
+        getMark,
+        getName
+    }
+ };
+
+
+//The game module 
 const gameModule = (() => {
+const _board = new Array(9).fill("");
 
-const boardgame = new Array(9).fill("x")
-
-const gameBoardDisplay = () => {
-    boards.forEach((board) => {
-        console.log(board)
-        board.textContent = boardgame[board.id]
-        console.log(board.id)
-    })
+const getBoard = () => {
+    return [..._board];
 }
+
+const BoardDisplay = () => {
+    let displayBoard = getBoard();
+    for (let i = 0; i <= cells.length - 1; i++) {
+        cells[i].textContent = displayBoard[i];
+        
+    }
+}
+
+const placeMarker = (index, marker) => {
+_board[index] = marker;
+BoardDisplay();
+}
+
+const boardReset = () => {
+    _board = new Array(9).fill("");
+}
+
+const checkWin = (marker) => {
+    const winCondition = [[0,1,2],[3,4,5],
+                         [6,7,8], [0,3,6],
+                         [1,4,7],[2,5,8],
+                         [0,4,8],[2,4,6]];
+
+    winCondition.forEach(condition => {
+        if(_board[condition[0]] === marker && _board[condition[1]] && _board[condition[2]]) {
+            return true;
+            console.log('yur');
+        }
+    })                  
+};
+
+
 return{
-    gameBoardDisplay
+    getBoard,
+    placeMarker,
+    boardReset,
+    BoardDisplay,
 }
 })();
+// console.log(gameModule.getBoard())
 
-console.log(gameModule.gameBoardDisplay())
-// console.log(gameModule)
+//the module that handles the flow of the game
+const game = (() => {
+    //players
+    const player1 = Player("you", "X");
+    const player2 = Player("computer", "O");
+
+    const currentPlayer = player1;
+
+    const switchPlayerTurn = () => {
+        if(currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    let turnsPlayed = 0;
+
+    const gamePlay = (e) => {
+    turnsPlayed++
+
+    let index = Array.prototype.indexOf.call(gameBoard.children, e.target)
+    gameModule.placeMarker(index, currentPlayer.getMark())
+
+    }
+
+    gameBoard.addEventListener('click', gamePlay)
+
+   return{
+       switchPlayerTurn,
+       currentPlayer,
+   }
+})();
+
+//    boards.forEach((board, index) => {
+//        board.addEventListener('click', () => {
+//         gameModule._board[index] = currentPlayer;
+//        console.log(gameModule._board)
+       
+//         //    alert('bro')
+//        })
+//    });
+
+// const gameBoardDisplay = () => {
+//     boards.forEach( (index, board) => {
+//         console.log(board)
+//         board.textContent = _board[index]
+//         console.log(board.id)
+//         console.log(index)
+//     })
+// }
